e942a64c4db03fec6f0cfad9a4b01db893950e07
rpollak96 <rpollak96@gmail.com>
2015-10-12 18:36:24 -0700

    way better

--- nats.py
+++ nats.py
@@ -8,7 +8,7 @@ nats.logger.setLevel(logging.ERROR)
 
 def getGameNumber():
 	for i in range(162):
-		date = datelist[i]
+		date = listofgames[i]
 		if (t.today().day<=date.day and t.today().month<=date.month):
 			return i
 	return 999
@@ -16,27 +16,23 @@ f = open('natsschedule.txt') #file containing information
 listofgames = []
 for line in f:
 	listofgames.append(line)
-splitlist = []
-for game in listofgames: 
-	splitlist.append(game.split('|'))
-datelist = []
-for game in splitlist:
-	datelist.append(t.strptime(game[0],"%m/%d/%y"))
+listofgames = map(lambda x: t.strptime(x[0],"%m/%d/%y"),\
+	map(lambda x:x.split('|'),listofgames))
 
 @nats.route('/')
 def index():
 	gamenum=getGameNumber()
-	try:
-		gameToday = (datelist[gamenum].day==t.today().day)
-	except:
-		return render_template('seasonover.html')
-	if gameToday:
-		return render_template('gameday.html', gameNumber=gamenum+1, opponent=(splitlist[gamenum][2]), gametime=(splitlist[gamenum][1]))
+	if gamenum != 999:
+		gameToday = (listofgames[gamenum].day==t.today().day)
+		if gameToday:
+			return render_template('gameday.html', gameNumber=gamenum+1, opponent=(splitlist[gamenum][2]), gametime=(splitlist[gamenum][1]))
+		else:
+			timetilgame = listofgames[gamenum]-t.today()
+			if timetilgame.days==0:
+				return render_template('gametomorrow.html', gameNumber=gamenum, opponent=(splitlist[gamenum][2]), gametime=(splitlist[gamenum][1]))
+			return render_template('nogame.html', days=timetilgame.days)	
 	else:
-		timetilgame = datelist[gamenum]-t.today()
-		if timetilgame.days==0:
-			return render_template('gametomorrow.html', gameNumber=gamenum, opponent=(splitlist[gamenum][2]), gametime=(splitlist[gamenum][1]))
-		return render_template('nogame.html', days=timetilgame.days)	
+		return render_template('seasonover.html')
 		
 if __name__=='__main__':
 	nats.debug=True
